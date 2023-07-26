90acacbda2050343e2368e89be2607032d3ef6be
marija-iloska <50119390+marija-iloska@users.noreply.github.com>
2023-07-26 15:55:44 -0400

    Making Regions Figures

--- Paper2/DataConversion/plot_coverages.m
+++ Paper2/DataConversion/plot_coverages.m
@@ -19,33 +19,39 @@ cov_mix{1}(end-5:end) = 10e-4;
 
 sz = 20;
 msz = 11;
-for t = 1 : N
+gr = [48, 219, 159]/256;
+for t = 1 : 1
 
 %      plot(time_mix{t}, movmean(cov_mix{t}, [0,1]), '.', 'MarkerSize', msz)
 %       hold on
 
-    plot(time_mix{t}, movmean(cov_mix{t}, [0,1]), 'Linewidth', 2)
+    plot(time_mix{t}, movmean(cov_mix{t}, [0,1]), 'k', 'Linewidth', 3)
+    hold on
+    xline(3, 'Color', 'm', 'linewidth', 3)
+    hold on
+    yline(0.31, 'Color', gr, 'linewidth', 3)
     hold on
 
-      cov_mix{t} = movmean(cov_mix{t}, [0,1]);
+      %x{t} = movmean(cov_mix{t}, [0,1]);
 
 end
 set(gca, 'FontSize', 15)
 xlabel('Time', 'FontSize', sz)
-ylabel('Coverages', 'FontSize', sz)
-legend( temps_strings, 'FontSize', sz)
-grid on
+ylabel('Coverage', 'FontSize', sz)
+%legend( temps_strings, 'FontSize', sz)
+legend( '\theta_{\tau}^X', 'FontSize', sz)
+%grid on
 
 % filename = 'figs/all_cov.eps';
 % print(gcf, filename, '-depsc2', '-r300');
 
 % Save for fitting
-save('Data/cov_time_for_fitting.mat', 'cov_mix', 'time_mix')
-
-filename = 'all_cov.csv';
-
-writecell(cov_mix', filename, 'Delimiter', ',');
-
-for col = 1:N
-    writecell(filename, cov_mix(:, col), 1, char('A' + col - 1));
-end
+% save('Data/cov_time_for_fitting.mat', 'cov_mix', 'time_mix')
+% 
+% filename = 'all_cov.csv';
+% 
+% writecell(cov_mix', filename, 'Delimiter', ',');
+% 
+% for col = 1:N
+%     writecell(filename, cov_mix(:, col), 1, char('A' + col - 1));
+% end
